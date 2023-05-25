7f1449987e2d158351fcfe201457f82338d70446
Sérgio Freire <sergiocarvalhotec@gmail.com>
2023-05-25 16:04:43 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,91 @@
+export const datafake = [
+  {
+    id: '1',
+
+    title: 'Total War: Pharaoh é anunciado e já tem previsão de lançamento',
+    description:
+      'Novo game da franquia de estratégia levará jogadores ao Novo Império Egípcio a partir de outubro',
+    photoCover:
+      'https://content.totalwar.com/total-war/com.totalwar.www2019/uploads/2023/05/18150719/twp_design_faq-16x9-1.jpg',
+  },
+  {
+    id: '2',
+
+    title: 'Gameplay de Mortal Kombat 1 será mostrada na Summer Game Fest 2023',
+    description:
+      'Ed Boon estará no palco do evento para revelar novidades na jogabilidade do game de luta',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/mortal_kombat_1_gameplay__c303l2x5.jpg',
+  },
+  {
+    id: '3',
+
+    title: 'Showrunner de The Witcher comenta saída de Henry Cavill: “luto”',
+    description:
+      'Lauren Schmidt Hissrich também falou sobre a possibilidade de acabar a série após a 3ª temporada',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/henry_cavill_the_witcher__a0225r50m-760x428.jpg',
+  },
+  {
+    id: '4',
+
+    title: 'Tudo o que rolou na PlayStation Showcase desta quarta-feira',
+    description:
+      'Marvel’s Spider Man 2, remake de Metal Gear Solid 3, Assassin’s Creed Mirage e mais!',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/tudo_o_que_rolou_playstation_showcase_v__2x3y2n6q-760x428.jpg',
+  },
+  {
+    id: '5',
+
+    title:
+      'Super Mario Bros, Pânico 6 e mais: confira todas as estreias do Amazon Prime Video em maio',
+    description: 'Produção fica atrás apenas dos dois filmes de Frozen',
+    photoCover:
+      'https://sm.ign.com/t/ign_br/screenshot/default/blob_gmck.640.jpg',
+  },
+  {
+    id: '6',
+
+    title: 'Diva do pop e do rock mundial, Tina Turner morre aos 83 anos ',
+    description: 'Estrela da música eternizou sucessos com a poderosa voz',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/tina_turner_mad_max__simx81h-760x428.jpg',
+  },
+  {
+    id: '7',
+
+    title: 'Arnold Schwarzenegger é anunciado como Chefe de Ação da Netflix',
+    description:
+      'Netflix anunciou Arnold Schwarzenegger como o novo Chefe do Departamento de Ação da empresa.',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/vitrine_schwarzenegger_chefe_de_acao_da_netflix_nerdbunker__30251l-760x428.jpg',
+  },
+  {
+    id: '8',
+
+    title: 'Aranhaverso 2 é chamado de “filme do ano” em primeiras reações',
+    description:
+      'Imprensa diz que nova animação do Homem-Aranha supera primeiro filme com história emocionante',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/homem_aranha_atraves_do_aranhaverso_primeiras_reacoes__2p60no13-1210x544.jpg',
+  },
+  {
+    id: '9',
+
+    title: 'Black Knight mistura Mad Max e Round 6 em ação competente',
+    description:
+      'Série coreana da Netflix é adaptação de quadrinho virtual de mesmo nome',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/black_knight_plagio_death_stranding__a1v4395-1210x544.jpg',
+  },
+  {
+    id: '10',
+
+    title: 'Série derivada de The Witcher terá Dolph Lundgren no elenco',
+    description:
+      'Dolph Lundgren (Rocky IV; Os Mercenários) será uma das estrelas da nova série derivada de The Witcher.',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/dolph__53iae2o0-1210x544.jpg',
+  },
+];
